6b1f2407c1c33b7fe724f0d033ad7a58e1cfd192
David <fridrich.david19@gmail.com>
2025-10-22 10:16:51 +0200

    index
    Signed-off-by: David <fridrich.david19@gmail.com>

--- node-custom/index.js
+++ node-custom/index.js
@@ -1,4 +1,10 @@
+/**
 const handle = async (context, body) => {
   return { body: 'first revision' };
 }
 module.exports = { handle };
+*/
+function invoke (context,body) {
+    return {body: 'first revision' };
+}
+module.exports = invoke;
